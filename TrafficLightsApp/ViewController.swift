68c87418ef01c40c79a921b727f67287fdf5a089
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-01-26 23:13:53 +0300

    Adding alpha for views and button corner radius

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
